6b46f55ba1110ba1e48138bea31b6372f1fb12a4
William ONeal <dev@weo3.com>
2019-03-13 10:46:33 -0700

    Major overhaul. Grid fixes. Flexbox fixes. Template structures.

--- src/app/shared/pipes/text-excerpt.pipe.ts
+++ src/app/shared/pipes/text-excerpt.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TextExcerptPipe implements PipeTransform {
 
   transform(value: string, limit?: number) {
-    const chars = limit ? limit : 500;
+    const chars = limit ? limit : 450;
     const trail = '...';
     return value.length > chars ? value.substring(0, chars) + trail : value + trail;
    }
