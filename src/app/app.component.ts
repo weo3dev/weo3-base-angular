07b06b3d99172611c9ab5e2a671f5baa3423537b
William ONeal <dev@weo3.com>
2019-11-07 08:12:58 +0000

    Adds vivus svg animation tool; adds site intro loading sequence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Vivus from 'vivus';
+import { longStackSupport } from 'q';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weo3-base-angular';
 
   sectionHeader: string;
+
+  ngOnInit() {
+
+    const v = new Vivus('weo3Logo', {
+      type: 'oneByOne',
+      duration: 200,
+      start: 'autostart',
+      delay: 0,
+      forceRender: true
+    }, this.logoExitView);
+
+  }
+
+  logoExitView() {
+    const logo = document.querySelector('#weo3Logo');
+    const bg = document.querySelector('.weo3-logo-container');
+
+    logo.classList.add('weo3-logo-exit');
+
+    setTimeout(() => {
+      bg.classList.add('weo3-container-exit');
+   }, 1500);
+
+    setTimeout(() => {
+     bg.parentNode.removeChild(bg);
+   }, 2000);
+  }
+
 
 }
